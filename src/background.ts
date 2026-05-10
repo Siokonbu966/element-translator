5c6206f3c3a5454beab07b268c0c54c877513e7b
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-05-10 22:09:25 +0900

    mv3に対応

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,26 @@
 import browser from "webextension-polyfill";
 
-browser.runtime.onMessage.addListener(async (msg: any) => {
+browser.runtime.onMessage.addListener(async (msg: any, sender:any) => {
   if (msg?.type === "START_CAPTURE") {
-    const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-    if (tab?.id != null) {
-      browser.tabs.sendMessage(tab.id, { type: "START_CAPTURE" });
+    const tabId = sender.tab?.id;
+
+    if (!tabId) return;
+
+    const url = sender.tab?.url ?? "";
+
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+      return;
+    }
+
+    try {
+      await browser.tabs.sendMessage(tabId, {
+        type: "START_CAPTURE",
+      });
+    } catch (error) {
+      console.warn("Faild to start capture in tab", error);
     }
   }
+
   if (msg.type === "SAVE_TRANSLATION_SOURCE") {
     console.log(msg);
     const { text, url } = msg.payload;
